ed76e37861e1944cf43d7e494b49715a66d32c8f
wm <wmakhoul1.csudh.edu>
2026-02-25 01:52:19 +0000

    wmakhoul7.py

---
+++ wmakhoul7.py
@@ -0,0 +1,23 @@
+
+x1 = 5
+x2 = 5.6
+x3 = "hello world"
+
+if x1 > 7:
+    print("x is less than 7")
+elif x1 == 7:
+    print("x is equal to 7")
+elif x1 < 7:
+    print("x is less than 7")
+
+if x2 > 5.6:
+    print("x is less than 5.6")
+elif x2 == 5.6:
+    print("x is equal to 5.6")
+elif x2 < 5.6:
+    print("x is less than 5.6")
+
+if x3 == "hello world":
+    print("hello wolrd")
+else:
+    print("x is not a string")
